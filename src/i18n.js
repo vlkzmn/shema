98a0ad715b624804ae4466b0a04b47752e6b8105
Volodymyr Kuzmin <vlkzmn@gmail.com>
2024-01-22 21:52:24 +0200

    fixed

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,26 @@
 import i18next from "i18next";
-import HttpBackend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
+// import HttpBackend from "i18next-http-backend";
+import detector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+// import { reactI18nextModule } from "react-i18next";
 
-i18next.use(HttpBackend).use(LanguageDetector).use(initReactI18next).init({
-  fallbackLng: "ua",
-});
+import translationUA from "./locales/ua/translation.json";
+import translationRU from "./locales/ru/translation.json";
+
+const resources = {
+  ua: {
+    translation: translationUA,
+  },
+  ru: {
+    translation: translationRU,
+  },
+};
+
+i18next
+  // .use(HttpBackend)
+  .use(detector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "ua",
+  });
