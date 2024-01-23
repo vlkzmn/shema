ca7d543c065b815d44e75069bed791414a594ac9
Volodymyr Kuzmin <vlkzmn@gmail.com>
2024-01-23 11:39:43 +0200

    fix

--- src/components/TestPage/Test.tsx
+++ src/components/TestPage/Test.tsx
@@ -10,7 +10,7 @@ import { Button } from '../Button/Button';
 export const Test = () => {
   // const { isDarkTheme } = useContext(DataContext);
   const [name, setName] = useState('');
-  const [user, setUser] = useState('default');
+  const [user, setUser] = useState('.');
   const [testing, setTesting] = useState(false);
   const { t } = useTranslation();
 
